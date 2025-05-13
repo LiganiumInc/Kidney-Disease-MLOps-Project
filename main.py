4fc9ade3b446d437ba490632d3cee4b94b4037b6
LiganiumInc <liganbernardin@gmail.com>
2025-05-13 18:37:12 +0100

    model evaluation and mlflow added

--- main.py
+++ main.py
@@ -34,16 +34,16 @@ from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
 
-# STAGE_NAME = "Training"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    model_trainer = ModelTrainingPipeline()
-#    model_trainer.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
